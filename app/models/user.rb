d4729a442fdb38d9b4879a7c0e1a10e6230c7857
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-08-07 10:12:56 +0000

    ログイン画面の認証修正とアルデーのデザイン反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "半角英数字とアンダースコアのみ使用できます" }
   validates :name, presence: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  generates_token_for :password_reset, expires_in: 15.minutes do
+    password_salt&.last(10)
+  end
 end
