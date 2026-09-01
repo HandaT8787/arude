cb84d29c23de56e74fdbd84d68db8b3373fe8dc5
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-01 08:56:43 +0000

    ログイン情報変更（username→email）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_by(username: params[:username], password: params[:password])
+    if user = User.authenticate_by(email_address: params[:email_address], password: params[:password])
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to home_url
     else
-      redirect_to new_session_path, alert: "ユーザーIDまたはパスワードが違います"
+      redirect_to new_session_path, alert: " メールアドレスまたはパスワードが違います"
     end
   end
 
