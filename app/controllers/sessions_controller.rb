415f4fe5cb6b72a075df69e4a8a4af339e92baab
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-04 09:29:42 +0000

    fix:ログイン先の仮ページ作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(email_address: params[:email_address], password: params[:password])
       start_new_session_for user
-      redirect_to home_url, notice: "ログインしました"
+      redirect_to home_path, notice: "ログインしました"
     else
       redirect_to new_session_path, alert: " メールアドレスまたはパスワードが違います"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :bookmarks, only: %i[create destroy]
   end
   get "sign_up", to: "users#new", as: :new_user
+  get "home", to: "home#top", as: :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
