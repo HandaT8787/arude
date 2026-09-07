b1aba6f32b0b6fea7a146ec9b0e90983408f968f
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-07 01:07:10 +0000

    fix/詳細画面レイアウト調整

--- app/javascript/post_swiper.js
+++ app/javascript/post_swiper.js
@@ -3,8 +3,9 @@ document.addEventListener("turbo:load", function(){
   if (!swiperEl) return;
 
   new Swiper(".post-swiper", {
+    autoplay: { delay: 4000, disableOnInteraction: false, waitForTransition: false },
     loop: true,
+    loopAdditionalSlides: 1,
     pagination: { el: ".swiper-pagination", clickable: true },
-    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
   });
 });
