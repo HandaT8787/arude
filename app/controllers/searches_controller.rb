86e45a8654b996e8804f38f3fc57341ef1c0638a
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-10 02:24:37 +0000

    feat/検索一覧画面作成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,5 +4,6 @@ class SearchesController < ApplicationController
     @results = @results.where("title LIKE :keyword OR description LIKE :keyword", keyword: "%#{params[:keyword]}%") if params[:keyword].present?
     @results = @results.where(category: params[:category]) if params[:category].present?
     @results = @results.joins(:tags).where(tags: { name: params[:tag_name] }) if params[:tag_name].present?
+    @results = @results.order(created_at: :desc).page(params[:page]).per(12)
   end
 end
