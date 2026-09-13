285a57326c395e5e0cfe9995e0ffb8909cdf22b1
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-13 15:16:37 +0000

    feat/地図検索機能

--- app/javascript/map_search.js
+++ app/javascript/map_search.js
@@ -6,13 +6,27 @@ document.addEventListener("turbo:load", function() {
     const postsData = JSON.parse(mapEl.dataset.posts);
     const centerLat = parseFloat(mapEl.dataset.centerLat);
     const centerLng = parseFloat(mapEl.dataset.centerLng);
-    
+
     const map = new google.maps.Map(mapEl, {
       center: { lat: centerLat, lng: centerLng },
       zoom: 12,
       mapTypeControl: false,
     });
 
+    const searchButton = document.getElementById("search-here-button");
+    if (searchButton) {
+      searchButton.addEventListener("click", function() {
+        const center = map.getCenter();
+        const lat = center.lat();
+        const lng = center.lng();
+
+        document.getElementById("map-search-lat").value = lat
+        document.getElementById("map-search-lng").value = lng
+        
+        searchButton.closest("form").submit();
+      });
+    };
+    
     postsData.forEach(function(post) {
       new google.maps.Marker({
         position: { lat: post.latitude, lng: post.longitude },
