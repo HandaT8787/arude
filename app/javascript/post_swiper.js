53be7c6660a0596a93835f61b328b7249ca88364
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-16 04:33:50 +0000

    fix/post-swiper console

--- app/javascript/post_swiper.js
+++ app/javascript/post_swiper.js
@@ -2,6 +2,8 @@ document.addEventListener("turbo:load", function(){
   const swiperEl = document.querySelector(".post-swiper");
   if (!swiperEl) return;
 
+  console.log("Swiper初期化開始");
+
   new Swiper(".post-swiper", {
     autoplay: { delay: 4000, disableOnInteraction: false, waitForTransition: false },
     rewind: true,
