9913634229fd6c6cba05ac4edd719abe9931c886
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-16 03:30:23 +0000

    fix/今日のアルデー呼び出し

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -10,12 +10,13 @@ class HomesController < ApplicationController
       if nearby_posts.size < 3
         nearby_posts = Post.where.not(latitude: nil, longitude: nil)
         .near([my_residence.latitude, my_residence.longitude], 20)
+        .to_a
       end
 
       if nearby_posts.size < 3
         @today_posts = Post.order(random_order).limit(3)
       else
-        @today_posts = nearby_posts.order(random_order).limit(3)
+        @today_posts = nearby_posts.sample(3)
       end
     else
       @today_posts = Post.order(random_order).limit(3)
