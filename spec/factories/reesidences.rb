6d11ac2486959491a9fc88e163b93f931e00c13e
HandaT8787 <289245591+HandaT8787@users.noreply.github.com>
2026-09-25 02:24:03 +0000

    実装フェーズ3完了

--- spec/factories/reesidences.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :reesidence do
-    user { nil }
-    prefecture { "MyString" }
-    city { "MyString" }
-    started_on { "2026-09-04" }
-    ended_on { "2026-09-04" }
-  end
-end
